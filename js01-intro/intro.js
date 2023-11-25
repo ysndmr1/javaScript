d60f5f831ab7cb94ee127dec8dcca335cad14ce0
ysndmr1 <ysndmr000@gmail.com>
2023-11-25 12:47:03 +0100

    first commit

---
+++ js01-intro/intro.js
@@ -0,0 +1,9 @@
+//*  JS INTRO   */
+
+// JS oop bir dildir %100 olmasa da kismen destekler, belirli temeller atip o temeller uzerinden turetilerek yazmayi saglar. Her seferinde sifirdan tasarlamaya gerek kalmiyor. Ilk yazilirken js browserlerda calisan bir dil yapmakti sonradan oop eklendi o yuzden oop destekler ama tam anlamiyla bir oop dili degildir.
+
+// Js browserlerin dili diye de adlandilir.
+
+// interpreter tabanli bir dil oldugu icin satir satir calistirarak ilerler, 4 satirda bir hata var ise ilk 3 satirin calisip 4 de hata verir. Compiler tabanli dillerde ise butun kod yazilir taranir ve hata olan satirlar duzeltilir. Bunun sebebi js browserlerdan bilgi cekerek isledigi icin veri geldikce isleniyor. Compiler tabanli diller kodu calisir halde kurulum uzerinden verir ve kullanici kodlari görmez kodlarin calisir halini alir kullanir.
+
+console.log("Hej");
