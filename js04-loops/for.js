f891dd28f116ea93cfe8c63154bbbacb051bb844
ysndmr1 <ysndmr000@gmail.com>
2023-12-11 16:34:11 +0100

    js

---
+++ js04-loops/for.js
@@ -0,0 +1,16 @@
+// * =======================================================
+// *             FOR LOOP
+// * =======================================================
+
+//* for syntax i ilk olarak let ile bir baslangic degeri ile baslar daha sonra bir kosul yazilir ve kosul true oldugu surece döngu devam eder daha sonra ise artim veya azalim miktari yazilir
+
+// //? Ornek: 1 den n kadar sayilari toplayan kodu yaziniz.
+
+// const n = Number(prompt("Enter a number"))
+
+// let sum = 0
+
+// for (let i = 1; i <= n; i++) {
+//   sum += i
+// }
+// console.log("SUM:", sum)
