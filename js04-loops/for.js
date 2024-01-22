d3a72acfe7d85a08914d084942b0787743b9b599
ysndmr1 <ysndmr000@gmail.com>
2024-01-22 13:29:08 +0100

    js

--- js04-loops/for.js
+++ js04-loops/for.js
@@ -70,4 +70,4 @@
 
 //? ORNEK: Kullanicidan 0-100 arasinda bir not isteyen ve girilen not
 //? 0-100'den farkli ise Kullaciniyi uyararak yeniden 0-100 arasinda
-//? not girmeye zorlayan kodu while dongusu ile yaziniz.
+//? not girmeye zorlayan kodu while dongusu ile yaziniz..
